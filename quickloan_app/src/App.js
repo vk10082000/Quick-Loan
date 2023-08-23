eaa61a531850bb7397081fe412cec22a33a946e7
Yashraj2607 <yr58907@gmail.com>
2023-08-23 23:49:42 +0530

    Navbar

--- quickloan_app/src/App.js
+++ quickloan_app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { MainRoutes } from './pages/MainRoutes';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <MainRoutes/>
     </div>
   );
