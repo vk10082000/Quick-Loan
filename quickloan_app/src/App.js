a537e855991da76d6f846304914ede32a067558a
Anburaj <anburaj2203@gmail.com>
2023-08-23 22:00:28 +0530

    created application page

--- quickloan_app/src/App.js
+++ quickloan_app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Application from './pages/Application';
 
 function App() {
   return (
     <div className="App">
-      
+      <Application/>
     </div>
   );
 }
