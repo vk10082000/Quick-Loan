6a1417120dbd75319f9940ff1ba71aa58303cf7c
Anburaj <anburaj2203@gmail.com>
2023-08-24 11:25:27 +0530

    added route for application

--- quickloan_app/src/pages/MainRoutes.jsx
+++ quickloan_app/src/pages/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Products from "./Products";
+import Application from "./Application";
 
 
 export const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/products" element={<Products/>} />
+        <Route path="/verification" element={<Application/>} />
      </Routes>
   );
 };
