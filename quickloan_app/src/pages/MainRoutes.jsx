420c50796f3314327c0c0a3b0b924ae963528f58
vk10082000 <vamshikrishna99089@gmail.com>
2023-08-26 00:28:19 +0530

    done

--- quickloan_app/src/pages/MainRoutes.jsx
+++ quickloan_app/src/pages/MainRoutes.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Products from "./Products";
 import Application from "./Application";
+import Bank from "./Bank";
 
 
 export const MainRoutes = () => {
   return (
     <Routes>
-        <Route path="/products" element={<Products/>} />
+        <Route path="/banks" element={<Products/>} />
         <Route path="/verification" element={<Application/>} />
+        <Route path="/bank/:bankname/:id" element={<Bank/>} />
      </Routes>
   );
 };
--- quickloan_app/src/pages/Products.jsx
+++ quickloan_app/src/pages/Products.jsx
@@ -1,25 +1,36 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getproducts } from '../redux/ProductRedux/action'
 import ProductCard from '../components/ProductCard'
-import {Navbar} from "../components/Navbar"
+
+import { SimpleGrid } from '@chakra-ui/react'
+
+
 export default function Products() {
-const {products,isLoading,isError} = useSelector((store)=> store.ProductReducer)
-const dispatch = useDispatch()
-useEffect(()=>{
-dispatch(getproducts)
-},[])
+  const { products, isLoading, isError } = useSelector((store) => store.ProductReducer)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getproducts)
+  }, [])
   return (
-      <DIV>
-        <Navbar/>
-        {isLoading && <h1>Loading...</h1>}
-        {isError && <h1>Something Wents to Wroung</h1>}
-        {products.map((product)=><ProductCard key={product.id} {...product} />)}
+    <DIV>
+
+      {isLoading && <h1>Loading...</h1>}
+      {isError && <h1>Something Wents to Wroung</h1>}
+      <SimpleGrid gap='30px' templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
+      {products.map((product) => <ProductCard key={product.id} {...product} />)}
+
+      </SimpleGrid>
+
     </DIV>
   )
 }
 
 const DIV = styled.div`
-  
-`
+
+  width: 90%;
+  margin:auto;
+  margin-top:10px;
+`;
